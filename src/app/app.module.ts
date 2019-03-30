b21307aa5ec6d1a4d37e2031e04010deba22bff3
Matt Needham <matthew.m.needham@gmail.com>
2019-03-30 17:21:22 -0400

    building out view-albums

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -58,7 +59,8 @@ import { ViewVoteSetsComponent } from './view-vote-sets/view-vote-sets.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
