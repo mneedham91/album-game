027f828b52c3b04ecc19947a95f2443ea875c84c
Matt Needham <matthew.m.needham@gmail.com>
2019-03-30 15:53:00 -0400

    undo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AlbumComponent } from './album/album.component';
-import { ArtistComponent } from './artist/artist.component';
-import { RoundComponent } from './round/round.component';
-import { TrackComponent } from './track/track.component';
-import { UserComponent } from './user/user.component';
-import { VoteSetComponent } from './vote-set/vote-set.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AlbumComponent,
-    ArtistComponent,
-    RoundComponent,
-    TrackComponent,
-    UserComponent,
-    VoteSetComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
