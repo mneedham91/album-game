b05b838d88e98ac9dd37cd817bcbd2165773f93f
Matt Needham <matthew.m.needham@gmail.com>
2019-03-30 17:31:13 -0400

    building view-album

--- src/app/view-album/view-album.component.ts
+++ src/app/view-album/view-album.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AlbumService } from '../album.service';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-view-album',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-album.component.css']
 })
 export class ViewAlbumComponent implements OnInit {
+  id: string;
+  album: Album;
 
-  constructor() { }
+  constructor(
+  	private router: Router, 
+  	private albumService: AlbumService, 
+  	private route: ActivatedRoute) { }
 
   ngOnInit() {
+  	this.route.params.subscribe(params => {
+  		this.id = params['id'];
+  	});
+  	this.albumService.getAlbum(this.id).subscribe(data => {
+  		this.album = data;
+  	});
   }
 
 }
