6da6ea63328427289c6f1486b4efaf938289d162
Matt Needham <matthew.m.needham@gmail.com>
2019-03-30 14:27:21 -0400

    test API endpoints

---
+++ module.factory.js
@@ -0,0 +1,355 @@
+var Factory = function(Schema, mongoose) {
+	this.Schema = Schema;
+	this.mongoose = mongoose;
+	
+	this.createSchemas = function() {
+		AlbumSchema = new this.Schema({
+			artist: Schema.ObjectId,
+			name: String,
+			nominator: Schema.ObjectId,
+			round: Schema.ObjectId,
+			year: Number
+		}, {timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}});
+		this.Album = mongoose.model('Album', AlbumSchema);
+		ArtistSchema = new this.Schema({
+			name: String
+		}, {timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}});
+		this.Artist = mongoose.model('Artist', ArtistSchema);
+		RoundSchema = new this.Schema({
+			name: String,
+			nominator: Schema.ObjectId,
+			number: Number
+		}, {timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}});
+		this.Round = mongoose.model('Round', RoundSchema);
+		TrackSchema = new this.Schema({
+			album: Schema.ObjectId,
+			name: String,
+			number: Number
+		}, {timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}});
+		this.Track = mongoose.model('Track', TrackSchema);
+		UserSchema = new this.Schema({
+			name: String,
+			password: String,
+			username: String
+		})
+		this.User = mongoose.model('User', UserSchema);
+		VoteSetSchema = new this.Schema({
+			album: Schema.ObjectId,
+			vote_one: Schema.ObjectId,
+			vote_two: Schema.ObjectId,
+			vote_three: Schema.ObjectId,
+			unfave: Schema.ObjectId,
+			user: Schema.ObjectId
+		}, {timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}});
+		this.VoteSet = mongoose.model('VoteSet', VoteSetSchema);
+	}
+
+	// Album Functions
+	this.getAlbum = function(id, res) {
+		this.Album.findById(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.getAlbums = function(query, res) {
+		this.Album.find(query, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.createAlbum = function(body, res) {
+		this.Album(body).save(function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.updateAlbum = function(id, body, res) {
+		this.Album.findByIdAndUpdate(id, body, {new: true}, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.deleteAlbum = function(id, res) {
+		this.Album.findByIdAndDelete(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	// Artist Functions
+	this.getArtist = function(id, res) {
+		this.Artist.findById(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.getArtists = function(query, res) {
+		this.Artist.find(query, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.createArtist = function(body, res) {
+		this.Artist(body).save(function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.updateArtist = function(id, body, res) {
+		this.Artist.findByIdAndUpdate(id, body, {new: true}, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.deleteArtist = function(id, res) {
+		this.Artist.findByIdAndDelete(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	// Round Functions
+	this.getRound = function(id, res) {
+		this.Round.findById(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.getRounds = function(query, res) {
+		this.Round.find(query, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.createRound = function(body, res) {
+		this.Round(body).save(function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.updateRound = function(id, body, res) {
+		this.Round.findByIdAndUpdate(id, body, {new: true}, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.deleteRound = function(id, res) {
+		this.Round.findByIdAndDelete(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	// Track Functions
+	this.getTrack = function(id, res) {
+		this.Track.findById(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.getTracks = function(query, res) {
+		this.Track.find(query, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.createTrack = function(body, res) {
+		this.Track(body).save(function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.updateTrack = function(id, body, res) {
+		this.Track.findByIdAndUpdate(id, body, {new: true}, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.deleteTrack = function(id, res) {
+		this.Track.findByIdAndDelete(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	// User Functions
+	this.getUser = function(id, res) {
+		this.User.findById(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.getUsers = function(query, res) {
+		this.User.find(query, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.createUser = function(body, res) {
+		this.User(body).save(function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.updateUser = function(id, body, res) {
+		this.User.findByIdAndUpdate(id, body, {new: true}, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.deleteUser = function(id, res) {
+		this.User.findByIdAndDelete(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	// VoteSet Functions
+	this.getVoteSet = function(id, res) {
+		this.VoteSet.findById(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.getVoteSets = function(query, res) {
+		this.VoteSet.find(query, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.createVoteSet = function(body, res) {
+		this.VoteSet(body).save(function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.updateVoteSet = function(id, body, res) {
+		this.VoteSet.findByIdAndUpdate(id, body, {new: true}, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+
+	this.deleteVoteSet = function(id, res) {
+		this.VoteSet.findByIdAndDelete(id, function(error, output) {
+			if (error) {
+				res.json(error);
+			} else {
+				res.json(output);
+			}
+		});
+	}
+	
+}
+
+module.exports = Factory;
---
+++ server.js
@@ -0,0 +1,168 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+var Factory = require('./module.factory.js');
+
+mongoose_options = {'useFindAndModify': false, 'useNewUrlParser': true};
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/albumdb', options=mongoose_options);
+var db = mongoose.connection;
+
+var factory = new Factory(Schema, mongoose);
+factory.createSchemas();
+
+var base_url = '/api/v1/'
+
+// Album Routes
+app.get(base_url + 'album', function(req, res) {
+	var resp = factory.getAlbums(req.query, res);
+});
+
+app.get(base_url + 'album/:id', function(req, res) {
+	var resp = factory.getAlbum(req.params.id, res);
+});
+
+app.post(base_url + 'album', function(req, res) {
+	var resp = factory.createAlbum(req.body, res);
+});
+
+app.patch(base_url + 'album/:id', function(req, res) {
+	var resp = factory.updateAlbum(req.params.id, req.body, res);
+});
+
+app.delete(base_url + 'album/:id', function(req, res) {
+	var resp = factory.deleteAlbum(req.params.id, res);
+});
+
+// Artist Routes
+app.get(base_url + 'artist', function(req, res) {
+	var resp = factory.getArtists(req.query, res);
+});
+
+app.get(base_url + 'artist/:id', function(req, res) {
+	var resp = factory.getArtist(req.params.id, res);
+});
+
+app.post(base_url + 'artist', function(req, res) {
+	var resp = factory.createArtist(req.body, res);
+});
+
+app.patch(base_url + 'artist/:id', function(req, res) {
+	var resp = factory.updateArtist(req.params.id, req.body, res);
+});
+
+app.delete(base_url + 'artist/:id', function(req, res) {
+	var resp = factory.deleteArtist(req.params.id, res);
+});
+
+// Round Routes
+app.get(base_url + 'round', function(req, res) {
+	var resp = factory.getRounds(req.query, res);
+});
+
+app.get(base_url + 'round/:id', function(req, res) {
+	var resp = factory.getRound(req.params.id, res);
+});
+
+app.post(base_url + 'round', function(req, res) {
+	var resp = factory.createRound(req.body, res);
+});
+
+app.patch(base_url + 'round/:id', function(req, res) {
+	var resp = factory.updateRound(req.params.id, req.body, res);
+});
+
+app.delete(base_url + 'round/:id', function(req, res) {
+	var resp = factory.deleteRound(req.params.id, res);
+});
+
+// Track Routes
+app.get(base_url + 'track', function(req, res) {
+	var resp = factory.getTracks(req.query, res);
+});
+
+app.get(base_url + 'track/:id', function(req, res) {
+	var resp = factory.getTrack(req.params.id, res);
+});
+
+app.post(base_url + 'track', function(req, res) {
+	var resp = factory.createTrack(req.body, res);
+});
+
+app.patch(base_url + 'track/:id', function(req, res) {
+	var resp = factory.updateTrack(req.params.id, req.body, res);
+});
+
+app.delete(base_url + 'track/:id', function(req, res) {
+	var resp = factory.deleteTrack(req.params.id, res);
+});
+
+// User Routes
+app.get(base_url + 'user', function(req, res) {
+	var resp = factory.getUsers(req.query, res);
+});
+
+app.get(base_url + 'user/:id', function(req, res) {
+	var resp = factory.getUser(req.params.id, res);
+});
+
+app.post(base_url + 'user', function(req, res) {
+	var resp = factory.createUser(req.body, res);
+});
+
+app.patch(base_url + 'user/:id', function(req, res) {
+	var resp = factory.updateUser(req.params.id, req.body, res);
+});
+
+app.delete(base_url + 'user/:id', function(req, res) {
+	var resp = factory.deleteUser(req.params.id, res);
+});
+
+// Vote Set Routes
+app.get(base_url + 'voteset', function(req, res) {
+	var resp = factory.getVoteSets(req.query, res);
+});
+
+app.get(base_url + 'voteset/:id', function(req, res) {
+	var resp = factory.getVoteSet(req.params.id, res);
+});
+
+app.post(base_url + 'voteset', function(req, res) {
+	var resp = factory.createVoteSet(req.body, res);
+});
+
+app.patch(base_url + 'voteset/:id', function(req, res) {
+	var resp = factory.updateVoteSet(req.params.id, req.body, res);
+});
+
+app.delete(base_url + 'voteset/:id', function(req, res) {
+	var resp = factory.deleteVoteSet(req.params.id, res);
+});
+
+// Catch-all Route
+app.use('/*',function(req, res) {
+    var resp = res.send('Server is running');
+});
+
+app.listen(process.env.PORT || 8080);
+
+db.on('error', function callback() {
+	console.log('Connection Error');
+});
+
+db.once('open', function callback() {
+	console.log('Mongo working!');
+});
