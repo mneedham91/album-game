0c4bcb438a00d2bc57628b03a198e3a8599e4910
Matt Needham <matthew.m.needham@gmail.com>
2019-03-31 18:10:40 -0400

    build out VoteSet service

--- src/app/vote-set.service.ts
+++ src/app/vote-set.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { VoteSet } from './vote-set';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoteSetService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  baseUrl: string = environment.baseUrl;
+
+  getVoteSets(query?: object) {
+  	return this.http.get<VoteSet[]>(this.baseUrl + 'voteset')
+  }
+
+  getVoteSet(id: string) {
+  	return this.http.get<VoteSet>(this.baseUrl + 'voteset/' + id);
+  }
+
+  createVoteSet(vote_set: VoteSet) {
+  	return this.http.post(this.baseUrl + 'voteset', vote_set);
+  }
+
+  editVoteSet(id: string, data: object) {
+  	return this.http.patch(this.baseUrl + 'voteset/' + id, data);
+  }
+
+  deleteVoteSet(id: string) {
+  	return this.http.delete(this.baseUrl + 'voteset/' + id);
+  }
 }
