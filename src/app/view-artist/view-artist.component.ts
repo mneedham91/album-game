fc78839b70b7300d9705e3f3a9dfadc83c26ca44
Matt Needham <matthew.m.needham@gmail.com>
2019-03-30 21:51:18 -0400

    build view-artist

--- src/app/view-artist/view-artist.component.ts
+++ src/app/view-artist/view-artist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ArtistService } from '../artist.service';
+import { Artist } from '../artist';
 
 @Component({
   selector: 'app-view-artist',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-artist.component.css']
 })
 export class ViewArtistComponent implements OnInit {
+  id: string;
+  artist: Artist;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private artistService: ArtistService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+  	this.route.params.subscribe(params => {
+  		this.id = params['id'];
+  	});
+  	this.artistService.getArtist(this.id).subscribe(data => {
+  		this.artist = data;
+  	});
   }
 
 }
