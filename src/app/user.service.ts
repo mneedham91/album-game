55ade8e9948cb9a610c6662b65621d3b0902340a
Matt Needham <matthew.m.needham@gmail.com>
2019-03-31 18:04:23 -0400

    build user service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from './user';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  baseUrl: string = environment.baseUrl;
+
+  getUsers(query?: object) {
+  	return this.http.get<User[]>(this.baseUrl + 'user')
+  }
+
+  getUser(id: string) {
+  	return this.http.get<User>(this.baseUrl + 'user/' + id);
+  }
+
+  createUser(user: User) {
+  	return this.http.post(this.baseUrl + 'user', user);
+  }
+
+  editUser(id: string, data: object) {
+  	return this.http.patch(this.baseUrl + 'user/' + id, data);
+  }
+
+  deleteUser(id: string) {
+  	return this.http.delete(this.baseUrl + 'user/' + id);
+  }
 }
