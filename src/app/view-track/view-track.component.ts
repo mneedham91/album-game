95ce3a9e5097b801f58edb916be5e549b985bbcb
Matt Needham <matthew.m.needham@gmail.com>
2019-03-31 18:02:50 -0400

    build out view-track

--- src/app/view-track/view-track.component.ts
+++ src/app/view-track/view-track.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { TrackService } from '../track.service';
+import { Track } from '../track';
 
 @Component({
   selector: 'app-view-track',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-track.component.css']
 })
 export class ViewTrackComponent implements OnInit {
+  id: string;
+  track: Track;
 
-  constructor() { }
+  constructor(
+  	private router: Router, 
+  	private trackService: TrackService, 
+  	private route: ActivatedRoute) { }
 
   ngOnInit() {
+  	this.route.params.subscribe(params => {
+  		this.id = params['id'];
+  	});
+  	this.trackService.getTrack(this.id).subscribe(data => {
+  		this.track = data;
+  	});
   }
 
 }
