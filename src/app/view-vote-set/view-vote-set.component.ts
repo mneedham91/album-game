c23e1e22c93ab5ebb8ff98f23c882530b0092edf
Matt Needham <matthew.m.needham@gmail.com>
2019-03-31 18:15:54 -0400

    build out view-vote-set

--- src/app/view-vote-set/view-vote-set.component.ts
+++ src/app/view-vote-set/view-vote-set.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { VoteSetService } from '../vote-set.service';
+import { VoteSet } from '../vote-set';
 
 @Component({
   selector: 'app-view-vote-set',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-vote-set.component.css']
 })
 export class ViewVoteSetComponent implements OnInit {
+  id: string;
+  voteSet: VoteSet;
 
-  constructor() { }
+  constructor(
+  	private router: Router, 
+  	private voteSetService: VoteSetService, 
+  	private route: ActivatedRoute) { }
 
   ngOnInit() {
+  	this.route.params.subscribe(params => {
+  		this.id = params['id'];
+  	});
+  	this.voteSetService.getVoteSet(this.id).subscribe(data => {
+  		this.voteSet = data;
+  	});
   }
 
 }
