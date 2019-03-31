0c611d5728e6ef89a892c608fd9a0b56bbe97704
Matt Needham <matthew.m.needham@gmail.com>
2019-03-31 15:47:54 -0400

    built view-rounds

--- src/app/view-rounds/view-rounds.component.ts
+++ src/app/view-rounds/view-rounds.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RoundService } from '../round.service';
+import { Round } from '../round';
 
 @Component({
   selector: 'app-view-rounds',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-rounds.component.css']
 })
 export class ViewRoundsComponent implements OnInit {
+  rounds: Round[];
 
-  constructor() { }
+  constructor(private roundService: RoundService, private router: Router) { }
 
   ngOnInit() {
+  	this.roundService.getRounds().subscribe(data => {
+  		this.rounds = data;
+  	});
+  }
+
+  viewRound(id: string): void {
+    this.router.navigate(['view-round', id])
   }
 
 }
