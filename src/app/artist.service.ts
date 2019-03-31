1bb33d430aa114cff8a02f8b32ef76d738b93417
Matt Needham <matthew.m.needham@gmail.com>
2019-03-30 21:43:42 -0400

    build artist service

--- src/app/artist.service.ts
+++ src/app/artist.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Artist } from './artst';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  baseUrl: string = environment.baseUrl;
+
+  getArtists(query?: object) {
+  	return this.http.get<Artist[]>(this.baseUrl + 'artist')
+  }
+
+  getArtist(id: string) {
+  	return this.http.get<Artist>(this.baseUrl + 'artist/' + id);
+  }
+
+  createArtist(artist: Artist) {
+  	return this.http.post(this.baseUrl + 'artist', artist);
+  }
+
+  editArtist(id: string, data: object) {
+  	return this.http.patch(this.baseUrl + 'artist/' + id, data);
+  }
+
+  deleteArtist(id: string) {
+  	return this.http.delete(this.baseUrl + 'artist/' + id);
+  }
 }
