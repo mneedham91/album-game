84623214f361ee33c723d921ee155d3750428933
Matt Needham <matthew.m.needham@gmail.com>
2019-04-04 10:16:04 -0400

    build out edit-vote-set

--- src/app/edit-vote-set/edit-vote-set.component.ts
+++ src/app/edit-vote-set/edit-vote-set.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { VoteSetService } from '../vote-set.service';
+import { VoteSet } from '../vote-set';
 
 @Component({
   selector: 'app-edit-vote-set',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-vote-set.component.css']
 })
 export class EditVoteSetComponent implements OnInit {
+  editVoteSetForm: FormGroup;
+  id: string;
+  vote_set: VoteSet;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+  	private voteSetService: VoteSetService,
+  	private route: ActivatedRoute,
+  	private router: Router) { }
 
   ngOnInit() {
+  	this.editVoteSetForm = this.formBuilder.group({
+  		album: '',
+  		user: '',
+  		vote_one: '',
+  		vote_two: '',
+  		vote_three: '',
+  		unfave: '',
+  	});
+  	this.route.params.subscribe(params => {
+  		this.id = params['id'];
+  	});
+  	this.voteSetService.getVoteSet(this.id).subscribe(data => {
+  		this.vote_set = data;
+  		this.editVoteSetForm.setValue({
+  			album: this.vote_set.album,
+  			user: this.vote_set.user,
+  			vote_one: this.vote_set.vote_one,
+  			vote_two: this.vote_set.vote_two,
+  			vote_three: this.vote_set.vote_three,
+  			unfave: this.vote_set.unfave,
+  		});
+  	});
+  	
+  }
+
+  onSubmit() {
+  	this.voteSetService.editVoteSet(this.id, this.editVoteSetForm.value).subscribe( data => {
+  	  this.router.navigate(['view-vote-set', this.id]);
+  	});
   }
 
 }
