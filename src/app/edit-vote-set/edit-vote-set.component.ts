4e5c6bc92f733b6c8e2daa0463e9cb5acd098d3b
Matt Needham <matthew.m.needham@gmail.com>
2019-04-04 13:54:08 -0400

    switch to dropdowns on edit-vote-set. dynamic title

--- src/app/edit-vote-set/edit-vote-set.component.ts
+++ src/app/edit-vote-set/edit-vote-set.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AlbumService } from '../album.service';
+import { TrackService } from '../track.service';
+import { Track } from '../track';
+import { UserService } from '../user.service';
 import { VoteSetService } from '../vote-set.service';
 import { VoteSet } from '../vote-set';
 
@@ -13,16 +18,21 @@ export class EditVoteSetComponent implements OnInit {
   editVoteSetForm: FormGroup;
   id: string;
   vote_set: VoteSet;
+  tracks: Track[];
 
-  constructor(private formBuilder: FormBuilder,
-  	private voteSetService: VoteSetService,
+  constructor(
+    private albumService: AlbumService,
+    private formBuilder: FormBuilder,
   	private route: ActivatedRoute,
-  	private router: Router) { }
+  	private router: Router, 
+    private titleService: Title,
+    private trackService: TrackService,
+    private userService: UserService,
+    private voteSetService: VoteSetService) { }
 
   ngOnInit() {
+    this.titleService.setTitle('Album Game | Edit Vote Set');
   	this.editVoteSetForm = this.formBuilder.group({
-  		album: '',
-  		user: '',
   		vote_one: '',
   		vote_two: '',
   		vote_three: '',
@@ -33,9 +43,22 @@ export class EditVoteSetComponent implements OnInit {
   	});
   	this.voteSetService.getVoteSet(this.id).subscribe(data => {
   		this.vote_set = data;
+      this.trackService.getTracks({album: this.vote_set.album}).subscribe(data => {
+        this.tracks = data.sort((a, b) => {
+          if (a.number < b.number) return -1;
+          else if (a.number > b.number) return 1;
+          else return 0;
+        });
+      });
+      this.albumService.getAlbum(this.vote_set.album).subscribe(data => {
+        this.vote_set.album = data.name;
+      });
+      this.userService.getUser(this.vote_set.user).subscribe(data => {
+        this.vote_set.user = data.name;
+      });
   		this.reset();
   	});
-  	
+    
   }
 
   onSubmit() {
@@ -46,8 +69,6 @@ export class EditVoteSetComponent implements OnInit {
 
   reset() {
     this.editVoteSetForm.setValue({
-      album: this.vote_set.album,
-      user: this.vote_set.user,
       vote_one: this.vote_set.vote_one,
       vote_two: this.vote_set.vote_two,
       vote_three: this.vote_set.vote_three,
