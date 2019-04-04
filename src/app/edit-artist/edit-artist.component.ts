36841d14e44cb34a8e5ac5c3b479b61802942fe9
Matt Needham <matthew.m.needham@gmail.com>
2019-04-04 09:32:53 -0400

    build out edit-artist

--- src/app/edit-artist/edit-artist.component.ts
+++ src/app/edit-artist/edit-artist.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ArtistService } from '../artist.service';
+import { Artist } from '../artist';
 
 @Component({
   selector: 'app-edit-artist',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-artist.component.css']
 })
 export class EditArtistComponent implements OnInit {
+  editArtistForm: FormGroup;
+  id: string;
+  artist: Artist;
 
-  constructor() { }
+  constructor(
+  	private formBuilder: FormBuilder,
+  	private artistService: ArtistService,
+  	private route: ActivatedRoute,
+  	private router: Router) { }
 
   ngOnInit() {
+  	this.editArtistForm = this.formBuilder.group({
+  		name: '',
+  	});
+  	this.route.params.subscribe(params => {
+  		this.id = params['id'];
+  	});
+  	this.artistService.getArtist(this.id).subscribe(data => {
+  		this.artist = data;
+  		this.editArtistForm.setValue({
+  			name: this.artist.name,
+  		});
+  	});
+  	
+  }
+
+  onSubmit() {
+  	this.artistService.editArtist(this.id, this.editArtistForm.value).subscribe( data => {
+  	  this.router.navigate(['view-artist', this.id]);
+  	});
   }
 
 }
