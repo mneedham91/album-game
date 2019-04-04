1db5afbc4863fa9d8fbeb214ef2031a198758384
Matt Needham <matthew.m.needham@gmail.com>
2019-04-04 09:59:44 -0400

    build out edit-album

--- src/app/edit-album/edit-album.component.ts
+++ src/app/edit-album/edit-album.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AlbumService } from '../album.service';
+import { Album } from '../album';
+import { DatePipe } from '@angular/common'
 
 @Component({
   selector: 'app-edit-album',
@@ -6,10 +11,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-album.component.css']
 })
 export class EditAlbumComponent implements OnInit {
+  editAlbumForm: FormGroup;
+  id: string;
+  album: Album;
 
-  constructor() { }
+  constructor(
+  	private formBuilder: FormBuilder,
+  	private albumService: AlbumService,
+  	private route: ActivatedRoute,
+  	private router: Router) { }
 
   ngOnInit() {
+  	this.editAlbumForm = this.formBuilder.group({
+  		artist: '',
+  		date: '',
+  		name: '',
+  		nominator: '',
+  		round: ''
+  	});
+  	this.route.params.subscribe(params => {
+  		this.id = params['id'];
+  	});
+  	this.albumService.getAlbum(this.id).subscribe(data => {
+  		this.album = data;
+  		let date = new DatePipe(navigator.language).transform(this.album.date, 'y-MM-dd');
+  		this.editAlbumForm.setValue({
+  			artist: this.album.artist,
+  			date: date,
+  			name: this.album.name,
+  			nominator: this.album.nominator,
+  			round: this.album.round
+  		});
+  	});
+  	
+  }
+
+  onSubmit() {
+  	let date = new DatePipe(navigator.language).transform(this.editAlbumForm.value['date'], 'MM-dd-y');
+  	this.editAlbumForm.controls['date'].setValue(date);
+  	this.albumService.editAlbum(this.id, this.editAlbumForm.value).subscribe( data => {
+  	  this.router.navigate(['view-album', this.id]);
+  	});
   }
 
 }
