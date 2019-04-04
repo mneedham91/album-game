0f8d25975a065243c4739e3b1cf7c1b7a57210ec
Matt Needham <matthew.m.needham@gmail.com>
2019-04-04 13:17:01 -0400

    make id fields user friendly on edit-album. also introduce dynamic titles

--- src/app/edit-album/edit-album.component.ts
+++ src/app/edit-album/edit-album.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AlbumService } from '../album.service';
 import { Album } from '../album';
+import { ArtistService } from '../artist.service';
+import { Artist } from '../artist';
+import { RoundService } from '../round.service';
+import { Round } from '../round';
+import { UserService } from '../user.service';
+import { User } from '../user';
 import { DatePipe } from '@angular/common'
 
 @Component({
@@ -14,14 +21,22 @@ export class EditAlbumComponent implements OnInit {
   editAlbumForm: FormGroup;
   id: string;
   album: Album;
+  artists: Artist[];
+  rounds: Round[];
+  users: User[];
 
   constructor(
   	private formBuilder: FormBuilder,
   	private albumService: AlbumService,
+    private artistService: ArtistService,
   	private route: ActivatedRoute,
-  	private router: Router) { }
+  	private router: Router,
+    private titleService: Title,
+    private roundService: RoundService,
+    private userService: UserService) { }
 
   ngOnInit() {
+    this.titleService.setTitle('Album Game | Edit Album');
   	this.editAlbumForm = this.formBuilder.group({
   		artist: '',
   		date: '',
@@ -36,7 +51,19 @@ export class EditAlbumComponent implements OnInit {
   		this.album = data;
   		this.reset();
   	});
-  	
+  	this.artistService.getArtists().subscribe(data => {
+      this.artists = data.sort((a, b) => {
+        if (a.name < b.name) return -1;
+        else if (a.name > b.name) return 1;
+        else return 0;
+      });
+    });
+    this.roundService.getRounds().subscribe(data => {
+      this.rounds = data;
+    });
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+    });
   }
 
   onSubmit() {
