aee9a8ed3eca3db4afa243959d7342eb55024670
Matt Needham <matthew.m.needham@gmail.com>
2019-04-04 19:55:00 -0400

    build out add-round

--- src/app/add-round/add-round.component.ts
+++ src/app/add-round/add-round.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RoundService } from '../round.service';
+import { UserService } from '../user.service';
+import { User } from '../user'
 
 @Component({
   selector: 'app-add-round',
@@ -7,12 +12,43 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./add-round.component.css']
 })
 export class AddRoundComponent implements OnInit {
+  addRoundForm: FormGroup;
+  users: User[];
+  roundNumber: number;
 
   constructor(
-  	private titleService: Title) { }
+  	private formBuilder: FormBuilder,
+  	private roundService: RoundService,
+  	private router: Router,
+  	private titleService: Title,
+  	private userService: UserService) { }
 
   ngOnInit() {
   	this.titleService.setTitle('Album Game | Add Round');
+  	this.roundService.getRounds().subscribe(data => {
+  		let sorted: any = data.sort((a, b) => {
+  			if (a.number < b.number) return 1;
+  			else if (a.number > b.number) return -1;
+  			else return 0
+  		});
+  		this.roundNumber = sorted[0].number + 1;
+  	});
+  	this.addRoundForm = this.formBuilder.group({
+  		name: '',
+  		description: '',
+  		nominator: '',
+  		number: ''
+  	});
+  	this.userService.getUsers().subscribe(data => {
+      this.users = data;
+    });
+  }
+
+  onSubmit() {
+  	this.addRoundForm.value['number'] = this.roundNumber;
+  	this.roundService.createRound(this.addRoundForm.value).subscribe( data => {
+  	  this.router.navigate(['view-rounds']);
+  	});
   }
 
 }
