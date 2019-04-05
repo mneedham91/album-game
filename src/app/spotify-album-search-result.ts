20794ebcf706555ce4469a6cfc68d59dc986d185
Matt Needham <matthew.m.needham@gmail.com>
2019-04-04 23:14:41 -0400

    fixed some type issues

--- src/app/spotify-album-search-result.ts
+++ src/app/spotify-album-search-result.ts
@@ -1,8 +1,49 @@
-import { SpotifyAlbum } from './spotify-album';
+//import { SpotifyAlbum } from './spotify-album';
+
+/*export class SpotifyAlbumSearchResult {
+	href: string;
+	items: SpotifyAlbum[];
+	limit: number;
+	next: any;
+	offset: number;
+	previous: any;
+	total: number
+}*/
 
 export class SpotifyAlbumSearchResult {
 	href: string;
-	items: SpotifyAlbum[];
+	items: [
+		{
+			album_type: string;
+			artists: [
+				{
+					external_urls: any;
+					href: string;
+					id: string;
+					name: string;
+					type: string;
+					uri: string;
+				}
+			]
+			available_markets: any;
+			external_urls: any;
+			href: string;
+			id: string;
+			images: [
+				{
+					height: number;
+					width: number;
+					url: string;
+				}
+			]
+			name: string;
+			release_date: string;
+			release_date_precision: string;
+			total_tracks: number;
+			type: string;
+			uri: string;
+		}
+	]
 	limit: number;
 	next: any;
 	offset: number;
