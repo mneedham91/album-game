0e8a335568d6fd731ee4a7f0ba91d28be90857ee
Matt Needham <matthew.m.needham@gmail.com>
2019-04-06 11:31:51 -0400

    add album from round

--- src/app/view-round/view-round.component.ts
+++ src/app/view-round/view-round.component.ts
@@ -14,6 +14,7 @@ import { UserService } from '../user.service';
   styleUrls: ['./view-round.component.css']
 })
 export class ViewRoundComponent implements OnInit {
+  currentRound: boolean;
   id: string;
   round: Round;
   albums: Album[];
@@ -37,7 +38,20 @@ export class ViewRoundComponent implements OnInit {
       this.userService.getUser(this.round.nominator).subscribe(data => {
         this.round.nominator = data.name
       });
+      this.roundService.getRounds().subscribe(data => {
+        let sorted: any = data.sort((a, b) => {
+          if (a.number < b.number) return 1;
+          else if (a.number > b.number) return -1;
+          else return 0
+        });
+        if (this.round._id == sorted[0]._id) {
+          this.currentRound = true;
+        } else {
+          this.currentRound = false;
+        }
+      });
   	});
+
     this.albumService.getAlbums({'round': this.id}).subscribe(data => {
       this.albums = data;
       for (let album of this.albums) {
