4aadf92701563f9918c9dd2c87d12a13ad7179eb
Matt Needham <matthew.m.needham@gmail.com>
2019-04-15 11:08:14 -0400

    migrating to image storage with AWS S3 for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: '/api/v1/',
-  images: 'http://s3.amazonaws.com/album-game/';
+  images: 'http://s3.amazonaws.com/album-game/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   baseUrl: 'http://localhost:8080/api/v1/',
-  images: '/assets/dev/';
+  images: '/assets/dev/',
 };
 
 /*
