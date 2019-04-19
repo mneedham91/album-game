93a313d73f69c82f1442be0258416e77b1c4a238
Matt Needham <matthew.m.needham@gmail.com>
2019-04-19 11:26:16 -0400

    take away sidebars for mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  deviceInfo = null;
+  isMobile: boolean;
   title = 'album-game';
+
+  constructor(private deviceService: DeviceDetectorService) { }
+
+  ngOnInit() {
+  	this.deviceInfo = this.deviceService.getDeviceInfo();
+  	this.isMobile = this.deviceService.isMobile();
+  }
 }
