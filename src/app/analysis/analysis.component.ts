3403dcbbaaaf9c5b73bd6bb53e12d8816869a78e
Matt Needham <matthew.m.needham@gmail.com>
2019-04-22 12:53:51 -0400

    building out analysis front end

--- src/app/analysis/analysis.component.ts
+++ src/app/analysis/analysis.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-analysis',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./analysis.component.css']
 })
 export class AnalysisComponent implements OnInit {
+  analysisForm: FormGroup;
+  choice: string;
+  options: string[];
+  users: User[];
+  x: string;
 
-  constructor() { }
+  constructor(
+  	private formBuilder: FormBuilder,
+  	private titleService: Title,
+  	private userService: UserService) { }
 
   ngOnInit() {
+  	this.titleService.setTitle('Album Game | Analysis');
+  	this.options = ['Faves', 'Unfaves', 'Disagreements'];
+  	this.userService.getUsers().subscribe(users => {
+  		this.users = users;
+  	});
+  	this.analysisForm = this.formBuilder.group({
+  		option: '',
+  		user_one: '',
+  		user_two: ''
+  	});
+  }
+
+  submit() {
+  	this.choice = this.analysisForm.value['option'];
+  	switch (this.choice) {
+  		case 'Faves':
+  			this.x = 'F';
+  			break;
+  		case 'Unfaves':
+  			this.x = 'U';
+  			break;
+  		case 'Disagreements':
+  			this.x = 'D';
+  			break;
+  	}
   }
 
 }
